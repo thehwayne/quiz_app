dcc35edacc89521ee8da295a9de95e9d50e9d4cc
thehwayne <thehwayne@gmail.com>
2025-02-25 11:45:08 -0500

    I have been working on this while in class alongside the professor. However, I have been having issues with creating a repo, some of it being my fault. This should fix everything hopefully.

---
+++ quiz_app/lib/quiz.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+  //Write other code to change the state
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Colors.blue,
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
---
+++ quiz_app/lib/start_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: Color.fromARGB(175, 255, 255, 255),
+          ),
+          SizedBox(
+            height: 80,
+          ),
+          Text(
+            "Learn Flutter the Fun Way",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            icon: const Icon(Icons.arrow_circle_right_outlined, size: 40),
+            onPressed: startQuiz,
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              padding: EdgeInsets.symmetric(
+                vertical: 20,
+                horizontal: 40,
+              ),
+            ),
+            label: Text(
+              "Start Quiz",
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
