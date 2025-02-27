6c689e48ff33870ea5d3bba99b9b3fa7ab48ae23
thehwayne <thehwayne@gmail.com>
2025-02-27 11:22:49 -0500

    Implemented questions.dart. Will add more questions.

---
+++ quiz_app/lib/data/questions.dart
@@ -0,0 +1,14 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'Who is the Scout\'s Father?',
+    [
+      'The Heavy',
+      'The Spy', //Correct Answer
+      'Tom Jones',
+      'Gordon Freeman'
+    ]
+  )
+
+];
