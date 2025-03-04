7ab39a0a38ba2f4a7a43f5f20f8d42ae23e30626
thehwayne <thehwayne@gmail.com>
2025-03-04 12:11:04 -0500

    Updates to results_screen, added question_summart.dart

--- quiz_app/lib/results_screen.dart
+++ quiz_app/lib/results_screen.dart
@@ -1,9 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/question_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key, required this.chosenAnswers});
+  const ResultsScreen({
+    super.key,
+    required this.chosenAnswers,
+  });
+
   final List<String> chosenAnswers;
-  
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].text,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': chosenAnswers[i],
+        },
+      );
+    }
+
+    return summary;
+  }
+
   @override
   Widget build(context) {
     return SizedBox(
@@ -17,7 +40,9 @@ class ResultsScreen extends StatelessWidget {
             const SizedBox(
               height: 30,
             ),
-            const Text('List of Answers and Questions'),//Replaced with all the answers/questions
+            QuestionsSummary(
+                summaryData:
+                    getSummaryData()), //Replaced with all the answers/questions
             const SizedBox(
               height: 30,
             ),
