27f46d0ff6df8af3e860f490de5b440df20ee430
thehwayne <thehwayne@gmail.com>
2025-03-04 11:02:15 -0500

    Added more questions

--- quiz_app/lib/data/questions.dart
+++ quiz_app/lib/data/questions.dart
@@ -1,6 +1,16 @@
 import 'package:quiz_app/models/quiz_question.dart';
 
 const questions = [
+        QuizQuestion(
+    'What are the teams RED and BLU fighting over?',
+    [
+      'A huge gold mine', 
+      'A mysterious asteroid filled with powerful gems',
+      'A useless plot of land with gravel', //Correct Answer
+      'A couple of oil refineries'  
+    ]
+  ),
+
   QuizQuestion(
     'Who is the Scout\'s Father?',
     [
@@ -17,7 +27,27 @@ const questions = [
       'Nothing special, it\'s a stock weapon',
       'Applies burn damage to enemies',
       'Allows a player to go invisible for longer if they don\'t move',
-      'Allows a player to feign death.'  //Correct Answer
+      'Allows a player to feign death'  //Correct Answer
+    ]
+  ),
+
+    QuizQuestion(
+    'Who is the Soldier\'s rather unique roommate?',
+    [
+      'Santa Claus',
+      'Poopy Joe',
+      'Merasmus the Magician', //Correct Answer
+      'Dolly Parton'  
+    ]
+  ),
+
+    QuizQuestion(
+    'What is the name of the company that sells both weapons and cosmetics in the TF2 universe?',
+    [
+      'Mann Co.', //Correct Answer
+      'Apple',
+      'Valve', 
+      'Aperture Science'  
     ]
   )
 
