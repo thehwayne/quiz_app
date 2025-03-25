ea6ab412c1244d4711dcd8f4083a5755fac4c6d6
thehwayne <thehwayne@gmail.com>
2025-03-24 21:50:47 -0400

    Visual changes to questions summary. Need to decide on a better color for the user_answer text

--- quiz_app/lib/questions_summary.dart
+++ quiz_app/lib/questions_summary.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsSummary extends StatelessWidget {
   const QuestionsSummary({super.key, required this.summaryData});
@@ -9,22 +10,68 @@ class QuestionsSummary extends StatelessWidget {
   Widget build(context) {
     return Column(
       children: summaryData.map((data) {
-        return Row(
-          children: [
-            Text(((data['question_index'] as int) +1).toString()),
-              Expanded(
-                child: Column(
-                  children:[
-                    Text(data['question'] as String),
-                    const SizedBox(height:5,),
-                    Text(data['user_answer'] as String),
-                    Text(data['correct_answer'] as String),
-                  ],
-                ),
-              )
-        ],
-      );
+        final isCorrect = data['user_answer'] == data['correct_answer'];
 
+        return Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // This is for the number circle thingy
+            Container(
+              width: 30,
+              height: 30,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: isCorrect ? Colors.lightBlueAccent: Colors.pink,
+                shape: BoxShape.circle,
+              ),
+              child: Text(
+                ((data['question_index'] as int) + 1).toString(),
+                style: GoogleFonts.lato(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+            const SizedBox(width: 20),
+
+
+            // And these are for the questions and answers
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    data['question'] as String,
+                    style: GoogleFonts.lato(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                    
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    data['user_answer'] as String,
+                    style: GoogleFonts.lato(
+                      color: Color(0xFFFF2EEA), 
+                      fontSize: 14,
+                      ),
+                    
+                  ),
+                  Text(
+                    data['correct_answer'] as String,
+                    style: GoogleFonts.lato(color: Colors.cyanAccent,
+                    fontSize: 14,
+                    
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                ],
+              ),
+            ),
+          ],
+        );
       }).toList(),
     );
   }
